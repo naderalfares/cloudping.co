89f956bac79a78e4f712c4f9969eaacff5e00d1f
Nader Alfares <nader.alfares@saihat.com>
2019-10-27 23:37:10 -0400

    added selected regions mapping

--- scripts/python3/latencies_to_csv.py
+++ scripts/python3/latencies_to_csv.py
@@ -2,36 +2,55 @@ import requests
 import json
 import sys
 
+
+
 if __name__ == "__main__":
+
+	try:
+		REGIONS_NAMES = json.load(open("regions.json", "r"))
+
+	except Exception as e:
+		print(e)
+
 	URL = "https://api.cloudping.co/averages"
 	r = requests.get(URL)
 	regions = []
+	b_selected = False
+
+
 	for reg in r.json():
 		regions.append(reg["region"])	
 
 	if len(sys.argv) == 1:
 		selected_regions = regions
-	elif sys.argv[1] == -1:
-		# add the selcted regions here
-		selected_regions = []
+	elif len(sys.argv) == 2 and sys.argv[1] == "-s":
+		selected_regions = list(REGIONS_NAMES.keys())
+		print(">>", selected_regions)
+		b_selected = True
 	else:
-		print("Usage: python3 " + sys.argv[0] + "[-s]\n\t-s: selected regions hardcoded in the script")
+		print("Usage: python3 " + sys.argv[0] + "[-s]\n\t-s: selected regions")
 		sys.exit(1)
 
 	with open("latencies.csv", "w") as fd:
 		for region in regions:
-			if reg["region"] not in selected_regions:
+			if region not in selected_regions:
 				continue
-			fd.write("," + region)
+			if b_selected:
+				fd.write("," + REGIONS_NAMES[region])
+			else:
+				fd.write("," + region)
 		fd.write("\n")
 
 		for index1, reg in enumerate(r.json()):
 			if reg["region"] not in selected_regions:
 				continue
 			assert(reg["region"] == regions[index1])
-			fd.write(regions[index1])
+			if b_selected:
+				fd.write(REGIONS_NAMES[regions[index1]])
+			else:
+				fd.write(regions[index1])
 			for index2 , avg in enumerate(reg["averages"]):
-				if reg["region"] not in selected_regions:
+				if avg["regionTo"] not in selected_regions:
 					continue
 				assert(avg["regionTo"] == regions[index2])
 				fd.write("," + str(int(avg["average"])))
